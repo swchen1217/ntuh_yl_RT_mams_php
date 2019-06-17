8906547ce32075a8b8ebe57f66d4726d59aeeb7a
swchen1217 <swchen1217@gmail.com>
2019-06-17 17:44:54 +0800

    re

--- change_pw.php
+++ change_pw.php
@@ -1,8 +1,66 @@
+<center>
+<h2>NTUH.YL 儀器管理系統</h2>
+<h3>更改登入密碼</h3>
 <?php
 require("config.php");
 
-$in = "";
-if(isset($_POST["in"]))
-	$in=$_POST["in"];
-echo $in;
+$tmppw = "";
+if(isset($_GET["tmppw"]))
+	$tmppw=$_GET["tmppw"];
+$acc = "";
+if(isset($_GET["acc"]))
+	$acc=$_GET["acc"];
+$input_acc = "";
+if(isset($_POST["input_acc"]))
+	$input_acc=$_POST["input_acc"];
+$input_old_pw = "";
+if(isset($_POST["input_old_pw"]))
+	$input_old_pw=$_POST["input_old_pw"];
+$input_new_pw = "";
+if(isset($_POST["input_new_pw"]))
+	$input_new_pw=$_POST["input_new_pw"];
+$input_new_pw_re = "";
+if(isset($_POST["input_new_pw_re"]))
+	$input_new_pw_re=$_POST["input_new_pw_re"];
+
+if($input_new_pw==""){
+    if($tmppw!="" && $acc!=""){
+    ?>
+        <form action="" method="post">
+            &emsp;員工編號：<input type="text" name="input_acc" required placeholder="請輸入員工編號(帳號)" value=<?php echo $acc; ?> disabled="disabled"><br><br>
+            &emsp;臨時密碼：<input type="text" name="input_tmppw" required placeholder="tmppw_**********" value=<?php echo $tmppw; ?> disabled="disabled" ><br><br>
+            &emsp;&emsp;新密碼：<input type="password" name="input_new_pw" required placeholder="請設定新密碼"><br><br>
+            確認新密碼：<input type="password" name="input_new_pw_re" required placeholder="請再次輸入新密碼"><br><br>
+            <input type="submit" value="確認更改">
+        </form>
+    <?php
+    }else{
+    ?>
+        <form action="" method="post">
+            &emsp;員工編號：<input type="text" name="input_acc" required placeholder="請輸入員工編號(帳號)"><br><br>
+            &emsp;&emsp;原密碼：<input type="text" name="input_old_pw" required placeholder="請輸入原密碼"><br><br>
+            &emsp;&emsp;新密碼：<input type="password" name="input_new_pw" required placeholder="請設定新密碼"><br><br>
+            確認新密碼：<input type="password" name="input_new_pw_re" required placeholder="請再次輸入新密碼"><br><br>
+            <input type="submit" value="確認更改">
+        </form>
+    <?php
+    }
+}else{
+    if($tmppw!=""){
+        $sql = 'SELECT application_time FROM `user_tmppw_tb` WHERE `account`="'.$acc.'" and `tmppw`="'.$tmppw.'" order by application_time desc';
+        $rs=mysqli_query($con,$sql);
+        if(mysqli_num_rows($rs) == 0){
+            echo "<font color=red><b>錯誤:請重新申請</b></font>";
+        }else{
+            list($time)=mysqli_fetch_row($rs);
+            echo $time;
+            echo (strtotime(date("Y-m-d H:i:s",time())) - strtotime($time));
+        }
+    }else{
+        
+    }
+    //echo $input_acc.$input_tmppw.$input_old_pw.$input_new_pw.$input_new_pw_re;
+}
+//echo $tmppw;
 ?>
+</center>
--- user.php
+++ user.php
@@ -82,7 +82,10 @@ if($mode=="forget_pw"){
     list($name,$acc_2)=mysqli_fetch_row($rs);
     $tmp_pw="tmppw_".substr(md5(uniqid(rand(), true)),0,10);
     
-    $forget_pw_mail_body = $name.' 你好<br>您的員工編號(帳號):'.$acc_2.'<br>請使用以下連結更改密碼<br>臨時密碼:'.$tmp_pw.'<br><font color=red>注意:臨時密碼與連結僅在<b>30分鐘</b>內有效,請在<b>30分鐘</b>內更改密碼,否則須重新申請</font><br>更改密碼連結:<a href="http://swchen1217.ddns.net/ntuh_yl_RT_mdms_php/change_pw.php?tmppw='.$tmp_pw.'">http://swchen1217.ddns.net/ntuh_yl_RT_mdms_php/change_pw.php?tmppw='.$tmp_pw.'</a>';
+    $forget_pw_mail_body = $name.' 你好<br>您的員工編號(帳號):'.$acc_2.'<br>請使用以下連結更改密碼<br>可在有效時間內使用臨時密碼登入儀器管理系統<br>臨時密碼:'.$tmp_pw.'<br><font color=red>注意:臨時密碼與連結僅在<b>30分鐘</b>內有效,請在<b>30分鐘</b>內更改密碼,否則須重新申請</font><br>更改密碼連結:<a href="http://swchen1217.ddns.net/ntuh_yl_RT_mdms_php/change_pw.php?acc='.$acc_2.'&tmppw='.$tmp_pw.'">http://swchen1217.ddns.net/ntuh_yl_RT_mdms_php/change_pw.php?acc='.$acc_2.'&tmppw='.$tmp_pw.'</a>';
+    
+    $sql2 = "insert into user_tmppw_tb(account,tmppw,application_time)values('".$acc_2."','".$tmp_pw."','".date("Y-m-d H:i:s",time())."')";
+    mysqli_query($con,$sql2);
     
     $mail = new PHPMailer(true);
     try {
