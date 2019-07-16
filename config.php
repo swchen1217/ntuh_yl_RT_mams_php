39d323479c7d80ef02d8206c187f6de8bb7161f1
Sz-Wei Chen <swchen1217@gmail.com>
2019-07-16 21:53:51 +0800

    re

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 date_default_timezone_set('Asia/Taipei');
 $mysql_host = "localhost";
-$mysql_user = "swc";
-$mysql_pass = "chen1217";
+$mysql_user = "server";
+$mysql_pass = "Gd94YaEioIe27MCM";
 $mysql_db   = "ntuh.yl_mdms";
 $con = mysqli_connect($mysql_host,$mysql_user,$mysql_pass,$mysql_db);
 /*if (!$con) 
