3bdec0338f8a87d67030b240c310867c9fd4ec10
Sz-Wei Chen <swchen1217@gmail.com>
2019-08-24 08:47:00 +0800

    re

--- make_qrcode.php
+++ make_qrcode.php
@@ -11,12 +11,12 @@ $new_qrcode_DID=$last_DID;
 $img_url='https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=MDMS.D001&qzone=2';
 $img = imagecreatefromstring(file_get_contents($img_url));
 putenv('GDFONTPATH=' . realpath('.'));
-$font = 'm.ttf';//字体文件
-$black = imagecolorallocate($img,255, 225, 255);//字体颜色 RGB
-$fontSize = 20;   //字体大小
-$circleSize = 0; //旋转角度
-$left = 50;      //左边距
-$top = 210;       //顶边距
+$font = 'm.ttf';
+$black = imagecolorallocate($img,255, 225, 255);
+$fontSize = 20;
+$circleSize = 0;
+$left = 50;
+$top = 210;
 imagefttext($img, $fontSize, $circleSize, $left, $top, $black, $font, '要加的文字内容');
 //ob_clean();
 //header('Content-Type:image/png');
