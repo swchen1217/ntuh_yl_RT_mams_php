216e312da6024378ae13a0e660b68d99f694adce
Sz-Wei Chen <swchen1217@gmail.com>
2019-09-01 22:27:28 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,15 +1,8 @@
 <?php
-/*$acc = "";
-if(isset($_REQUEST["acc"]))
-    $acc=$_REQUEST["acc"];*/
-$pw = "";
-if (isset($_REQUEST["pw"]))
-    $pw = $_REQUEST["pw"];
-
 $acc = filter_input(INPUT_POST, "acc");
 if (!$acc) $acc = filter_input(INPUT_GET, "acc");
 
-$db = new PDO('mysql:host=localhost;dbname=ntuh.yl_mdms;charset=utf8', 'server', 'Gd94YaEioIe27MCM');
+require("config2.php");
 
 $sql = 'SELECT * FROM user_tb where account=:acc';
 $rs = $db->prepare($sql);
