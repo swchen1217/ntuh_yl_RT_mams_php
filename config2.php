ecc5d7e6a45bc35e0c66d5b42222da267ed86a17
Sz-Wei Chen <swchen1217@gmail.com>
2019-09-01 22:24:01 +0800

    Update config2.php

--- config2.php
+++ config2.php
@@ -5,10 +5,6 @@ $mysql_user = "server";
 $mysql_pass = "Gd94YaEioIe27MCM";
 $mysql_db = "ntuh.yl_mdms";
 $con = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
-/*if (!$con) 
-	//die("連接錯誤: " . iconv('gbk', 'utf-8//IGNORE', mysqli_connect_error())); 
-	die("連接錯誤"); 
-else
-	echo("伺服器連線成功");*/
+
 mysqli_query($con, "set names utf8");
 ?>
