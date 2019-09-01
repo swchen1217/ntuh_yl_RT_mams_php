8df8ffc3ecd7abd61c088d2ec6cd313ea46618ff
Sz-Wei Chen <swchen1217@gmail.com>
2019-09-01 12:30:09 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,7 +1,16 @@
 <?php
+$acc = "";
+if(isset($_REQUEST["acc"]))
+    $acc=$_REQUEST["acc"];
+$pw = "";
+if(isset($_REQUEST["pw"]))
+    $pw=$_REQUEST["pw"];
+
 $db = new PDO('mysql:host=localhost;dbname=ntuh.yl_mdms;charset=utf8', 'server', 'Gd94YaEioIe27MCM');
-$rs=$db->query('SELECT * FROM user_tb');
+$sql='SELECT * FROM user_tb where account="'.$acc.'"';
+$rs=$db->prepare($sql);
+$rs->execute(array(':acc'=>$acc));
 while ($row=$rs->fetch(PDO::FETCH_NUM)){
-    echo $row[0]."<br>";
+    echo $row[1]."<br>";
 }
 ?>
