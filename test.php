1ed7428cb7a1d81ca7a0fe52a607e49ea9ff1523
Sz-Wei Chen <swchen1217@gmail.com>
2019-09-01 22:42:20 +0800

    re

--- test.php
+++ test.php
@@ -9,6 +9,7 @@ $sql = 'SELECT * FROM user_tb where account=:acc';
 $rs = $db->prepare($sql);
 $rs->bindValue(':acc', $acc, PDO::PARAM_STR);
 $rs->execute();
+
 while ($row = $rs->fetch(PDO::FETCH_NUM)) {
     echo $row[1] . "<br>";
 }
