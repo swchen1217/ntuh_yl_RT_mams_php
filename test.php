c22483aceb237251b14fa8442622857a9fa32e32
Sz-Wei Chen <swchen1217@gmail.com>
2019-09-01 22:35:46 +0800

    Update test.php

--- test.php
+++ test.php
@@ -12,10 +12,4 @@ while ($row = $rs->fetch(PDO::FETCH_NUM)) {
     echo $row[1] . "<br>";
 }
 
-function requst($key) {
-    $tmp = filter_input(INPUT_POST, $key);
-    if (!$tmp) $tmp = filter_input(INPUT_GET, $key);
-    return $tmp;
-}
-
 ?>
