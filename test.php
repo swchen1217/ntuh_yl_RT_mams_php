3d1c6d7eaaf735e03de0714cddecd836613ef05e
Sz-Wei Chen <swchen1217@gmail.com>
2019-09-01 12:01:32 +0800

    test

--- test.php
+++ test.php
@@ -1,5 +1,7 @@
 <?php
 $db = new PDO('mysql:host=localhost;dbname=ntuh.yl_mdms;charset=utf8', 'server', 'Gd94YaEioIe27MCM');
 $rs=$db->query('SELECT * FROM user_tb');
-while ($rs)
+while ($row=$rs->fetch(PDO::FETCH_NUM)){
+    echo $row[0]."<br>";
+}
 ?>
