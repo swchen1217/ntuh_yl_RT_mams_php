bde38ffb4a4dcbc84fd1190d93656dae6b7a9c7b
Sz-Wei Chen <swchen1217@gmail.com>
2019-09-02 01:22:36 +0800

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ date_default_timezone_set('Asia/Taipei');
 
 $mysql_host = "localhost";
 $mysql_user = "server";
-$mysql_pass = "Gd94YaEioIe27MCM";
+$mysql_pass = "Cqx3OauoSwmH7y17";
 $mysql_db = "ntuh.yl_mdms";
 
 $db = new PDO('mysql:host='.$mysql_host.';dbname='.$mysql_db.';charset=utf8', $mysql_user, $mysql_pass);
