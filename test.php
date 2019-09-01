e9aa105d1db9820541bb3dec03198288774a13b0
Sz-Wei Chen <swchen1217@gmail.com>
2019-09-01 12:49:13 +0800

    Update test.php

--- test.php
+++ test.php
@@ -1,15 +1,20 @@
 <?php
-$acc = "";
+/*$acc = "";
 if(isset($_REQUEST["acc"]))
-    $acc=$_REQUEST["acc"];
+    $acc=$_REQUEST["acc"];*/
 $pw = "";
 if(isset($_REQUEST["pw"]))
     $pw=$_REQUEST["pw"];
 
+$acc = filter_input(INPUT_POST, "acc");
+if(!$acc) $acc = filter_input(INPUT_GET, "acc");
+
 $db = new PDO('mysql:host=localhost;dbname=ntuh.yl_mdms;charset=utf8', 'server', 'Gd94YaEioIe27MCM');
-$sql='SELECT * FROM user_tb where account="'.$acc.'"';
+
+$sql='SELECT * FROM user_tb where account=:acc';
 $rs=$db->prepare($sql);
-$rs->execute(array(':acc'=>$acc));
+$rs->bindValue(':acc',$acc,PDO::PARAM_STR);
+$rs->execute();
 while ($row=$rs->fetch(PDO::FETCH_NUM)){
     echo $row[1]."<br>";
 }
