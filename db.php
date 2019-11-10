c2ff8ef423447ca2e2f70e2826c442b70fd2f5e0
Sz-Wei Chen <swchen1217@gmail.com>
2019-11-10 13:26:29 +0800

    Update db.php

--- db.php
+++ db.php
@@ -4,6 +4,7 @@
 
 require("config.php");
 require("request.php");
+require("UserCheck.php");
 
 mb_internal_encoding('UTF-8');
 
@@ -102,24 +103,6 @@ if ($mode == "update_device_tb_storeroom") {
         echo "user_error";
     }
     exit;
-}
-function UserCheck($acc_in, $pw_in, $permission_in, PDO $mDB)
-{
-    if ($acc_in != "" && $pw_in != "") {
-        $sql = "SELECT password,permission FROM `user_tb` WHERE `account`=:acc";
-        $rs = $mDB->prepare($sql);
-        $rs->bindValue(':acc', $acc_in, PDO::PARAM_STR);
-        $rs->execute();
-        list($pw_r, $permission_r) = $rs->fetch(PDO::FETCH_NUM);
-        if($pw_r==$pw_in){
-            if($permission_r!='0'){
-                return true;
-            }else
-                return false;
-        }else
-            return false;
-    } else
-        return false;
 }
 
 ?>
