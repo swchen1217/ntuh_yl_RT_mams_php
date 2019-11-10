0a1e0ec03264178cda393ecd9b3ec47d34d959f4
Sz-Wei Chen <swchen1217@gmail.com>
2019-11-10 13:07:02 +0800

    Update user.php

--- user.php
+++ user.php
@@ -113,50 +113,6 @@ if ($mode == "forget_pw") {
         }
         exit;
     }
-
-
-    // OLD
-    /*$sql = 'SELECT name,account FROM `user_tb` WHERE `email`=:email';
-    $rs = $db->prepare($sql);
-    $rs->bindValue(':email', $email, PDO::PARAM_STR);
-    $rs->execute();
-    list($name, $acc_2) = $rs->fetch(PDO::FETCH_NUM);
-    $tmp_pw = "tmppw_" . substr(md5(uniqid(rand(), true)), 0, 8);
-
-    $forget_pw_mail_body = $name . ' 你好<br>員工編號(帳號):' . $acc_2 . '<br>請使用以下連結更改密碼<br>可在有效時間內使用臨時密碼登入醫療儀器管理系統<br>臨時密碼:' . $tmp_pw . '<br><font color=red>注意:臨時密碼與連結僅在<b>30分鐘</b>內有效,請在<b>30分鐘</b>內更改密碼,否則須重新申請</font><br><font color=red>注意:新密碼不允許以"<b>tmppw_</b>"為開頭</font><br>更改密碼連結:<a href="http://swchen1217.ddns.net/ntuh_yl_RT_mdms_php/change_pw.php?acc=' . $acc_2 . '&tmppw=' . $tmp_pw . '">http://swchen1217.ddns.net/ntuh_yl_RT_mdms_php/change_pw.php?acc=' . $acc_2 . '&tmppw=' . $tmp_pw . '</a>';
-
-    $sql2 = "insert into user_tmppw_tb(account,tmppw,application_time)values(:acc_2,:tmp_pw,:d)";
-    $rs2 = $db->prepare($sql2);
-    $rs2->bindValue(':acc_2', $acc_2, PDO::PARAM_STR);
-    $rs2->bindValue(':tmp_pw', $tmp_pw, PDO::PARAM_STR);
-    $rs2->bindValue(':d', date("Y-m-d H:i:s", time()), PDO::PARAM_STR);
-    $rs2->execute();
-
-    $mail = new PHPMailer(true);
-    try {
-        //$mail->SMTPDebug = 3;
-        $mail->isSMTP();
-        $mail->Charset = 'UTF-8';
-        $mail->Host = 'ssl://smtp.gmail.com:465';
-        $mail->SMTPAuth = true;
-        $mail->Username = MAIL_USER;
-        $mail->Password = MAIL_PASS;
-        $mail->setFrom('ntuhyl.mdms@gmail.com', 'NTUH.YL 醫療儀器管理系統');
-        $mail->addAddress($email);
-        $mail->isHTML(true);
-        $mail->Subject = 'NTUH.YL 儀管系統 忘記密碼';
-        $mail->Body = $forget_pw_mail_body;
-        $mail->send();
-        echo '寄信成功';
-        if ($redirection == "true") {
-            header('Location: http://swchen1217.ddns.net/ntuh_yl_RT_mdms_php/change_pw.php?redirection_ok=true&acc=' . $acc_2);
-            exit;
-        }
-    } catch (Exception $e) {
-        echo 'Message could not be sent.';
-        echo 'Mailer Error: ' . $mail->ErrorInfo;
-    }
-    exit;*/
 }
 if ($mode == "get_create_time") {
     $sql = 'SELECT `created` FROM `user_tb` WHERE `account`=:acc';
