50f4f2821e408bd34ec4eb5344698d19c95eea75
Sz-Wei Chen <swchen1217@gmail.com>
2019-12-01 15:01:13 +0800

    Update make_qrcode.php

--- make_qrcode.php
+++ make_qrcode.php
@@ -13,16 +13,16 @@ $new_qrcode_DID=$last_DID;*/
 
 $img_url='./img/a01.png';
 $img = imagecreatefromstring(file_get_contents($img_url));
-putenv('GDFONTPATH=' . realpath('.'));
-$font = 'arial';
-$black = imagecolorallocate($img,255, 225, 255);
-$fontSize = 20;
+//putenv('GDFONTPATH=' . realpath('.'));
+$font = 'C:\Windows\Fonts\arial.ttf';
+$black = imagecolorallocate($img,0, 0, 0);
+$fontSize = 15;
 $circleSize = 0;
-$left = 50;
-$top = 210;
+$left = 63;
+$top = 247;
 imagettftext($img, $fontSize, $circleSize, $left, $top, $black, $font, 'MDMS.D0001');
 
-//header('Content-Type: image/png');
+header('Content-Type: image/png');
 
 imagepng($img);
 imagedestroy($img);
