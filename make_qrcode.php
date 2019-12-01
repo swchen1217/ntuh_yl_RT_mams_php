65e5c7520c0fdc6b0fefd65906ba514ec4351831
Sz-Wei Chen <swchen1217@gmail.com>
2019-12-01 18:14:09 +0800

    Update make_qrcode.php

--- make_qrcode.php
+++ make_qrcode.php
@@ -1,13 +1,13 @@
 <?php
 //mkqr("MDMS.D0005");
 
-/*for ($i = 1; $i <= 30; $i++) {
+for ($i = 1; $i <= 30; $i++) {
     $s = $i;
     if ($i < 10)
         $s = '0' . $i;
     mkqr("MDMS.D00" . $s);
     mkqr("MDMS.D05" . $s);
-}*/
+}
 
 function mkqr($data)
 {
@@ -27,14 +27,23 @@ function mkqr($data)
     QRcode::png($data, $tmp_url, 'H', 3, 4);
     $img = imagecreatefromstring(file_get_contents($tmp_url));
     $font = 'C:\Windows\Fonts\arial.ttf';
+    //$font = 'C:\Windows\Fonts\mingliu.ttc';
+    //$font = 'C:\Windows\Fonts\msjh.ttf';
     $black = imagecolorallocate($img, 0, 0, 0);
     $fontSize = 8;
     $circleSize = 0;
     $left = 10;
     $top = 85;
     imagettftext($img, $fontSize, $circleSize, $left, $top, $black, $font, $data);
-    /*//test
-    Header("Content-type: image/png");
+
+    $fontSize2 = 8;
+    $circleSize2 = 0;
+    $left2 = 19;
+    $top2 = 10;
+    imagettftext($img, $fontSize2, $circleSize2, $left2, $top2, $black, $font, "NTUH.YL");
+
+    //test
+    /*Header("Content-type: image/png");
     imagepng($img);*/
     imagepng($img, $opt_url);
     imagedestroy($img);
