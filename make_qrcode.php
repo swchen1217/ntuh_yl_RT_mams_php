363a43e69bf30694026bb1d0bec443fb26b69ee2
Sz-Wei Chen <swchen1217@gmail.com>
2019-12-01 16:16:04 +0800

    Update make_qrcode.php

--- make_qrcode.php
+++ make_qrcode.php
@@ -1,32 +1,26 @@
 <?php
+mkqr("MDMS.D1234");
 
-require("config.php");
-include ('./phpqrcode/qrlib.php');
+function mkqr($data){
+    include ('./phpqrcode/qrlib.php');
 
-/*$data="";
-QRcode::png('MDMS.D0001','./img/output/test.png','H',3,4);*/
+    $tmp_url='./img/tmp/'.$data.'.png';
+    QRcode::png('MDMS.D0001',$tmp_url,'H',3,4);
+    $img = imagecreatefromstring(file_get_contents($tmp_url));
+    $font = 'C:\Windows\Fonts\arial.ttf';
+    $black = imagecolorallocate($img,0, 0, 0);
+    $fontSize = 8;
+    $circleSize = 0;
+    $left = 10;
+    $top = 85;
+    imagettftext($img, $fontSize, $circleSize, $left, $top, $black, $font, $data);
+    /*//test
+    Header("Content-type: image/png");
+    imagepng($img);*/
+    imagepng($img,'./img/deviceqrcode/'.$data.'.png');
+    imagedestroy($img);
 
-
-
-
-
-$img_url='./img/a02.png';
-$img = imagecreatefromstring(file_get_contents($img_url));
-$font = 'C:\Windows\Fonts\arial.ttf';
-$black = imagecolorallocate($img,0, 0, 0);
-$fontSize = 8;
-$circleSize = 0;
-$left = 10;
-$top = 85;
-imagettftext($img, $fontSize, $circleSize, $left, $top, $black, $font, 'MDMS.D0001');
-
-Header("Content-type: image/png");
-
-imagepng($img);
-//imagepng($img,'img/output/a02.png');
-imagedestroy($img);
-
-//header('Location:./img/output/a01.png');
-
-//cho '<img src="./img/output/a02.png" />';10
+    //header('Location:./img/deviceqrcode/'.$data.'.png');
+    echo '<img src="./img/deviceqrcode/'.$data.'.png" />';
+}
 ?>
