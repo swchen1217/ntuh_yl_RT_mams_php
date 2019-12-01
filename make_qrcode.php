ed5b7973f8de45122602f5f792dcf2b1ded58ca7
Sz-Wei Chen <swchen1217@gmail.com>
2019-12-01 15:58:02 +0800

    Update make_qrcode.php

--- make_qrcode.php
+++ make_qrcode.php
@@ -1,31 +1,32 @@
 <?php
 
-//ini_set('display_errors','off');
-
 require("config.php");
+include ('./phpqrcode/qrlib.php');
 
-$new_qrcode_DID="";
+/*$data="";
+QRcode::png('MDMS.D0001','./img/output/test.png','H',3,4);*/
 
- /*$sql = ' SELECT `DID` FROM `device_tb` WHERE 1 ORDER BY `DID` DESC';
-$rs=mysqli_query($con,$sql);
-list($last_DID)=mysqli_fetch_row($rs);
-$new_qrcode_DID=$last_DID;*/
 
-$img_url='./img/a01.png';
+
+
+
+$img_url='./img/a02.png';
 $img = imagecreatefromstring(file_get_contents($img_url));
-//putenv('GDFONTPATH=' . realpath('.'));
 $font = 'C:\Windows\Fonts\arial.ttf';
 $black = imagecolorallocate($img,0, 0, 0);
-$fontSize = 15;
+$fontSize = 8;
 $circleSize = 0;
-$left = 63;
-$top = 247;
+$left = 10;
+$top = 85;
 imagettftext($img, $fontSize, $circleSize, $left, $top, $black, $font, 'MDMS.D0001');
 
-imagepng($img,'img/output/a01.png');
+Header("Content-type: image/png");
+
+imagepng($img);
+//imagepng($img,'img/output/a02.png');
 imagedestroy($img);
 
 //header('Location:./img/output/a01.png');
 
-echo '<img src="./img/output/a01.png" />';
+//cho '<img src="./img/output/a02.png" />';10
 ?>
